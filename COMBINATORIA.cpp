b4e12c6b784b64bfdf6469f816271fa26c8aa22f
Fernando Leonel Moschen <64292259+ferchumoschen@users.noreply.github.com>
2022-10-25 01:16:35 -0300

    Add files via upload

---
+++ COMBINATORIA.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+int main(){
+    int i,n,pri=0,primos=0, divisor = 1, divisores = 0, num = 0;
+    for (i=1; i<=10; i++){
+        cout << "Ingrese un numero: ";
+        cin >> n;
+        if(num % divisor == 0){
+divisores++;
+}
+while(divisor <= num);
+if(divisores == 2){
+cout<<"n-> El numero "<<n<<" es PRIMO.";
+}else{
+cout<<"n-> El numero "<<n<<" NO es PRIMO.";
+}
+
+}
+    system("pause");
+return 0;
+}
